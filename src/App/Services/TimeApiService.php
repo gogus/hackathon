aedd21951465f5efef99b1bfb89879d18977a351
sirdawid <sirdawid@gazeta.pl>
2017-03-11 18:34:37 +0100

    bug fix

--- src/App/Services/TimeApiService.php
+++ src/App/Services/TimeApiService.php
@@ -20,6 +20,6 @@ class TimeApiService implements \App\Domain\Service\ServiceInterface
     public function ask( $query)
     {
         // TODO: Implement ask() method.
-        return file_get_contents('http://www.timeapi.org/utc/now?format=%25a%20%25b%20%25d%20%25I:%25M:%25S%20%25Y');
+        return date('Y-m-d H:i:s');
     }
 }
